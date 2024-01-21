3235b4bdb9db39a585aca6d2df5f700982c863c3
Gustavo Bender <gustavo.benderr@gmail.com>
2024-01-21 00:24:39 -0300

    r2 question from DMOJ

---
+++ questions/r2/README.md
@@ -0,0 +1,10 @@
+# R2 
+
+<br>
+refs: "https://dmoj.ca/problem/coci06c2p1"
+
+
+<br>
+
+## Solução
+Apenas inverter a função
---
+++ questions/r2/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define int ll
+
+int32_t main() {
+  int r1, s;
+  cin >> r1 >> s;
+  cout << 2 * s - r1 << endl;
+
+  return 0;
+}
