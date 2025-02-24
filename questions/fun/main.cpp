e6ff1352fa8d251057f9387c5d48a1d94aab47a4
Gustavo Bender <gustavo.benderr@gmail.com>
2025-02-24 14:18:00 -0300

    add: question fun from codeforces

---
+++ questions/fun/main.cpp
@@ -0,0 +1,49 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+
+
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+using ll = long long;
+#define int ll
+using mint = atcoder::modint;
+
+void solution() {
+  int n,x;
+  cin >> n >> x;
+  int counter = 0;
+  for(int i = 1; i <= n && i < x; i++) {
+    for(int j = 1; i * j <= n && i+j < x; j++) {
+      counter += min((n-i*j)/(i+j), x - i -j);
+    }
+  }
+  cout << counter << endl;
+
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  atcoder::modint::set_mod(998244353);
+  int t;
+  cin >> t;
+  while(t--)
+    solution();
+
+  return 0;
+}
