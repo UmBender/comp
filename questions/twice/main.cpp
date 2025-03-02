6e686994362b5e92af66ef89a3b8bd064a30bba6
Gustavo Bender <gustavo.benderr@gmail.com>
2025-03-02 15:22:05 -0300

    add: question twice from codeforces

---
+++ questions/twice/main.cpp
@@ -0,0 +1,56 @@
+//#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+
+
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+using ll = long long;
+#define int ll
+//using mint = atcoder::modint;
+
+void solution() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(auto &i: a) {
+		cin >> i;
+	}
+	int counter = 0;
+	vector<int> ca(n+1);
+	for(auto i: a) {
+		ca[i]++;
+	}
+	sort(ca.rbegin(), ca.rend());
+	for(int i = 0; i < ca.size() ;i++) {
+		counter += ca[i] /2;
+	}
+	cout << counter << endl;
+  
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+//  atcoder::modint::set_mod(998244353);
+  int t;
+  cin >> t;
+  while(t--)
+    solution();
+
+  return 0;
+}
