49ad65ae577ae379544b5e5722824e986139abf0
Gustavo Bender <gustavo.benderr@gmail.com>
2025-03-12 16:40:40 -0300

    add: question cirno and operations from codeforces

---
+++ questions/cirno_and_operations/main.cpp
@@ -0,0 +1,66 @@
+// #include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+using ll = long long;
+#define int ll
+// using mint = atcoder::modint;
+
+
+
+int a[1010];
+void solution() {
+  int n;
+	cin >> n;
+  for (int i = 0; i < n; i++) {
+		cin >> a[i];
+  }
+	int ans = -1E18;
+	int sum = 0; 
+	for(int i = 0; i < n; i++) {
+		sum += a[i];
+	}
+	ans = max(ans, sum);
+
+	while(n > 1) {
+		for(int i = 0; i < n;i++) {
+			a[i] = a[i] - a[i+1];
+		}
+		sum = 0;
+		n--;
+		for(int i = 0; i < n;i++) {
+			sum += a[i];
+		}
+		ans= max(ans,max(sum,-sum));
+	}
+
+	cout << ans << endl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  // atcoder::modint::set_mod(998244353);
+  int t;
+  cin >> t;
+  while (t--)
+    solution();
+
+  return 0;
+}
