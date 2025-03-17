c187cfd1b98a1f344177ed8f266287c2f1833686
Gustavo Bender <gustavo.benderr@gmail.com>
2025-03-16 22:05:03 -0300

    add: question binary matrix from codeforces

---
+++ questions/binary_matrix/main.cpp
@@ -0,0 +1,67 @@
+// clang-format off
+//#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+using ll = long long;
+#define int ll
+//using mint = atcoder::modint;
+// clang-format on
+
+void solution() {
+    int n, m;
+    cin >> n >> m;
+    vector<string> grid(n);
+    for (auto &i : grid) {
+        cin >> i;
+    }
+    vector<int> lines(n), columns(m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == '1') {
+                lines[i]++;
+                columns[j]++;
+            }
+        }
+    }
+
+    int counter_lines = 0, counter_columns = 0;
+    for (auto &i : lines) {
+        i %= 2;
+        if (i) {
+            counter_lines++;
+        }
+    }
+    for (auto &j : columns) {
+        j %= 2;
+        if (j) {
+            counter_columns++;
+        }
+    }
+    cout << max(counter_columns, counter_lines) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    // atcoder::modint::set_mod(998244353);
+    int t;
+    cin >> t;
+    while (t--)
+        solution();
+
+    return 0;
+}
