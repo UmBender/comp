42680c149fcf3bb998561c1c24909ceb4c584ca0
Gustavo Bender <gustavo.benderr@gmail.com>
2025-06-24 12:31:51 +0200

    add: question tak and hotels from atcoder

---
+++ questions/tak_and_hotels/a.cpp
@@ -0,0 +1,42 @@
+// clang-format off
+//#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+using ll = long long;
+using ld = long double;
+#define int ll
+//using mint = atcoder::modint;
+// clang-format on
+
+void solution() {
+	ll n, k, x, y;
+	cin >> n >> k >> x >> y;
+	int first_night = min(n, k);
+	int restn = n - first_night;
+	int second_night = restn;
+	dbg(first_night, second_night);
+	cout << first_night * x + second_night * y << endl;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	// atcoder::modint::set_mod(998244353);
+	solution();
+
+	return 0;
+}
