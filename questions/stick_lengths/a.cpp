ad9d0fd429bf16313a60a4bc9a86279f0b430aa1
Gustavo Bender <gustavo.benderr@gmail.com>
2025-08-30 15:16:13 +0200

    add: question stick lengths from cses

---
+++ questions/stick_lengths/a.cpp
@@ -0,0 +1,69 @@
+// clang-format off
+//#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+#define makeconst(value) []( return (value);)
+using ll = long long;
+using ld = long double;
+#define int ll
+using i64 = long long; using u64 = unsigned long long; using f32 = float; using f64 = double; using f128  = long double;
+//using mint = atcoder::modint;
+// clang-format on
+
+void solution() {
+	int n;
+	cin >> n;
+	vector<int> p(n);
+	for (auto &i : p) { cin >> i; }
+	auto cost = [&](int value) -> int {
+		int acc = 0;
+		for (auto i : p) { acc += abs(i - value); }
+		return acc;
+	};
+	int left = 0;
+	int right = 1E9 + 100;
+	int minIndex = -1;
+	while (left <= right) {
+		int mid1 = left + (right - left) / 3;
+		int mid2 = right - (right - left) / 3;
+		int firstCost = cost(mid1);
+		int secondCost = cost(mid2);
+
+		if (firstCost == secondCost) {
+			left = mid1 + 1;
+			right = mid2 - 1;
+			minIndex = mid1;
+
+		} else if (firstCost < secondCost) {
+			right = mid2 - 1;
+			minIndex = mid1;
+		} else {
+			left = mid1 + 1;
+			minIndex = mid2;
+		}
+	}
+	cout << cost(minIndex) << endl;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	// atcoder::modint::set_mod(998244353);
+	solution();
+
+	return 0;
+}
