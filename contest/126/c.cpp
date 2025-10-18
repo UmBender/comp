260c535c2c9c30a16488fe647669da536db3a7f4
Gustavo Bender <gustavo.benderr@gmail.com>
2025-10-18 15:42:47 -0300

    add: contest atcoder beginner contest 428 from atcoder

---
+++ contest/126/c.cpp
@@ -0,0 +1,86 @@
+// clang-format off
+//#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void __dbg() { cerr << endl; }
+template <typename T> void __dbg(T t) { cerr << t << endl; }
+template <typename T, typename... TRest> void __dbg(T first, TRest... rest) {
+  cerr << first << ", ";
+  __dbg(rest...);
+}
+#define dbg(...)                                                               \
+  do {                                                                         \
+    cerr << "DBG> " << "(" << #__VA_ARGS__ << ") = ";                          \
+    __dbg(__VA_ARGS__);                                                        \
+  } while (0)
+#define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
+#define makeconst(value) []() { return (value); }
+using ll = long long;
+using ld = long double;
+#define int ll
+using i64 = long long; using u64 = unsigned long long; using f32 = float; using f64 = double; using f128  = long double;
+#define ssort(x) ranges::sort(x)
+#define mkunique(x) ranges::sort(x), x.erase(unique(x.begin(), x.end()), x.end()), x.shrink_to_fit()
+//using mint = atcoder::modint;
+// clang-format on
+
+void solution();
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	// atcoder::modint::set_mod(998244353);
+	// clang-format off
+	solution();
+	// clang-format on
+
+	return 0;
+}
+
+void solution() {
+	int q;
+	cin >> q;
+	vector<char> stack;
+	vector<bool> okay;
+	auto first_query = [&](char c) -> bool {
+		if (!stack.empty() && stack.back() == '(' && c == ')') {
+			okay.push_back(true);
+			stack.pop_back();
+		} else {
+			okay.push_back(false);
+			stack.push_back(c);
+		}
+		return stack.empty();
+	};
+
+	auto second_query = [&]() -> bool {
+		if (!okay.empty() && okay.back()) {
+			okay.pop_back();
+			stack.push_back('(');
+		} else {
+			okay.pop_back();
+			stack.pop_back();
+		}
+		return stack.empty();
+	};
+	for (int qq = 0; qq < q; qq++) {
+		int opt;
+		cin >> opt;
+		if (opt == 1) {
+			char c;
+			cin >> c;
+			if (first_query(c)) {
+				cout << "Yes" << endl;
+			} else {
+				cout << "No" << endl;
+			}
+		} else {
+			if (second_query()) {
+				cout << "Yes" << endl;
+			} else {
+				cout << "No" << endl;
+			}
+		}
+	}
+}
