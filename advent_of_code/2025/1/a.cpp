a67a0c026f44219aa0422f7ff74965346d50a3e8
Gustavo Bender <gustavo.benderr@gmail.com>
2025-12-02 20:47:32 -0300

    add: advent of code 2025 day 1

---
+++ advent_of_code/2025/1/a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i32 = int32_t;
+using i64 = int64_t;
+using i128 = __int128;
+using VI = vector<i32>;
+using VVI = vector<VI>;
+using PII = pair<i32,i32>;
+using VII = vector<PII>;
+using VVII = vector<VII>;
+
+
+i32 main() {
+	cin.tie(0)->sync_with_stdio(0);
+	i32 n;
+	cin >> n;
+	i64 actual = 50;
+	i32 counter = 0;	
+	constexpr i64 times = 100 * ((i64)1e8);
+	while(n--){
+		string a;
+		cin >> a;
+		i64 direction;
+		if(a[0] == 'R') {
+			direction = 1;
+		}else{
+			direction = -1;
+		}
+		a.erase(a.begin());
+		i64 result = stoll(a);
+		actual += times;
+		actual += result * direction;
+		actual %= 100;
+		if(actual == 0) {
+			counter++;
+		}
+	}
+	cout << counter << endl;
+	return 0;
+}
